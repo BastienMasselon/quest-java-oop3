936bcd9ebf7c62494a6d7acca25f53ea77adbfd4
Mudy Mud <bastien.masselon@laposte.net>
2024-12-18 15:17:59 +0100

    Challenge quete Interface

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -7,6 +7,48 @@ public class Eagle extends Bird {
         super(name);
         this.flying = false;
         this.altitude = 0;
+    }
+
+    @Override
+    public void takeOff() {
+        if (!this.flying && this.altitude == 0) {
+            this.flying = true;
+            System.out.println(this.getName() + " takes off in the sky.");
+        }
+    }
+
+    public int ascend(int meters) {
+        if (this.flying) {
+            this.altitude = Math.min(this.altitude + meters, 4000);
+            System.out.println(this.getName() + " flies upward, altitude : " + this.altitude);
+        }
+        return this.altitude;
+    }
+
+    @Override
+    public int descend(int meters) {
+        if (this.flying) {
+            this.altitude = Math.max(this.altitude - meters, 0);
+            System.out.println(this.getName() + " flies downward, altitude : " + this.altitude);
+        }
+        return this.altitude;
+    }
+
+    @Override
+    public void glide() {
+        if (this.flying) {
+            System.out.println("glides into the air.");
+        }
+    }
+
+    @Override 
+    public void land() {
+        if (this.flying && this.altitude <= 1) {
+            this.altitude = 0;
+            System.out.println(this.getName() + " lands on the ground.");
+        } else {
+            System.out.println(this.getName() + " is too high, it can't land.");
+        }
     }
 
     public int getAltitude() {
